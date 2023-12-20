e4b30a6aeea3d42074e002d56023ba2d1730eec5
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-12-20 12:08:14 +0300

    refactor: added new request-url

--- src/globals.ts
+++ src/globals.ts
@@ -1,1 +1,2 @@
-export const DEFAULT_URL = 'http://localhost:7070'
+// export const DEFAULT_URL = 'http://localhost:7070'
+export const DEFAULT_URL = 'https://ra-react-router-task-2-crud-backend.onrender.com'
